4c5bc1594722c33510516cb789fdfca93dff1c75
Willamar Fernandes <wfs20@hotmail.com>
2014-06-14 21:51:45 -0300

    ExamRef 70-483

---
+++ Capitulo 01-2/CancelingTasks/CancelingTasks-003/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelingTasks_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+
+                token.ThrowIfCancellationRequested();
+
+            }, token).ContinueWith((t) =>
+            {                
+                //t.Exception.Handle((e) => true);
+                Console.WriteLine("You have canceled the task");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Console.WriteLine("Press enter to stop the task");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Press enter to end the application");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Capitulo 01-2/CancelingTasks/CancelingTasks-004/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelingTasks_004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task longRunning = Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+            });
+
+            
+            //Metodo seta um timeout para a task
+            int index = Task.WaitAny(new[] { longRunning }, 1000);
+
+            if (index == -1)
+            {
+                Console.WriteLine("Task timed out");
+            }
+            Console.Read();
+        }
+    }
+}
