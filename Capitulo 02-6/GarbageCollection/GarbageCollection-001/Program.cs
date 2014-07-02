3200da397a1db954df6cc69a29075c15a1f166ef
Willamar Fernandes <wfs20@hotmail.com>
2014-07-02 20:37:39 -0300

    Exam ref 70-483

---
+++ Capitulo 02-6/GarbageCollection/GarbageCollection-001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollection_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class SomeType
+    {
+        ~SomeType()
+        {
+            //this code id called when the finalize method executes
+        }
+    }
+}
---
+++ Capitulo 02-6/GarbageCollection/GarbageCollection-002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollection_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter stream = File.CreateText("temp.dat");
+            stream.Write("some data");
+            GC.Collect();
+
+            GC.WaitForPendingFinalizers();
+            File.Delete("temp.dat");
+        }
+    }
+
+    
+}
